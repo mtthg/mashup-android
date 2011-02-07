0f348fe90e6793274022723bb1ce74561b4b2497
Jon McKay <jonathan.mckay@students.olin.edu>
2011-02-07 14:29:24 -0500

    Made Custom ListView and created instructions for main page

--- margiejon-android/Cold-Ice629/src/com/mashup/bboard/BulletinBoard.java
+++ margiejon-android/Cold-Ice629/src/com/mashup/bboard/BulletinBoard.java
@@ -16,26 +16,30 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.AdapterView.OnItemSelectedListener;
 
-public class BulletinBoard extends ListActivity implements OnItemSelectedListener {
+public class BulletinBoard extends Activity implements OnItemSelectedListener {
     /** Called when the activity is first created. */
 	ArrayList <String> Classes;
 	static final Integer classIndex = 13;
 	TextView title;
-	//TableLayout layout;
+	ListView listview;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //title = (TextView)findViewById(R.id.tv);
-        //title.setText("Please Choose a Course");
+        setContentView(R.layout.main);
+        title = (TextView)findViewById(R.id.tv);
+        title.setText("Please Choose a Course");
+        listview = (ListView)findViewById(R.id.list);
+        
         
         HttpClient httpClient = new DefaultHttpClient();
 		HttpGet getMethod = new HttpGet("http://cold-ice-629.heroku.com/courses.json");
@@ -62,7 +66,7 @@ public class BulletinBoard extends ListActivity implements OnItemSelectedListene
        
         Collections.sort(Classes);
         ArrayAdapter<String> classAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Classes);
-        setListAdapter(classAdapter);
+        listview.setAdapter(classAdapter);
         
     }
     
